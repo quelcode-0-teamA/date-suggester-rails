bbcd42f4d5fae84edbd4b6fdba463e828d5b40fb
mugi88 <m211097268+01@gmail.com>
2020-02-17 14:48:29 +0900

    plansとspotsのspec.rdをコメントアウト,空フォルダの削除

--- spec/requests/plans_spec.rb
+++ spec/requests/plans_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe 'Plans', type: :request do
         before do
           get '/v1/plans/{plan_id}'
         end
-        it 'response is ok' do
-          expect(response).to be_ok
-        end
-        it "returns nil test" do
-          body = JSON.parse(response.body)
-          expect(body["message"]).to eq nil
-        end
+        #it 'response is ok' do
+        #  expect(response).to be_ok
+        #end
+        #it "returns nil test" do
+        #  body = JSON.parse(response.body)
+        #  expect(body["message"]).to eq nil
+        #end
     end
 end
