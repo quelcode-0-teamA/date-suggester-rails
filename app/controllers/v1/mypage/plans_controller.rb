99a40ffd3cf0d6931e5c38584e27524750d7231e
mugi88 <m211097268+01@gmail.com>
2020-02-20 01:24:47 +0900

    Add save!

---
+++ app/controllers/v1/mypage/plans_controller.rb
@@ -0,0 +1,34 @@
+module V1
+  module Mypage
+    class PlansController < ApplicationController
+      before_action :authorize!
+      before_action :set_myplan
+
+      def create
+        myplan = MyPlan.new(user_id: @current_user.id)
+        myplan.save!
+        render_serializer(@myplan, MyPlanSerializer)
+      end
+
+      def show
+        render_serializer(@myplan, MyPlanSerializer)
+      end
+
+      def destroy
+        myplan.destroy!
+        render json: { 'message': 'デートプランを削除しました' }
+      end
+
+      private
+
+        def index
+          @myplans = MyPlan.all
+        end
+
+        def set_myplan
+          @myplan = MyPlan.find(params[:id])
+        end
+
+    end
+  end
+end
