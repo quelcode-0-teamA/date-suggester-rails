caeb95fdbcbd0a2f2b3bdd4b3ee124b3ff88bfa9
necocoa <joyds217@gmail.com>
2020-02-21 18:47:25 +0900

    Add validate test for user update

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,64 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user, :with_area) }
-  describe 'Validates' do
-    it 'shold be valid' do
-      expect(user).to be_valid
-    end
-    describe 'name' do
-      it 'should be proper long' do
-        user.name = 'a' * 20
+  describe 'create' do
+    let(:user) { build(:user, :with_area) }
+    describe 'Validates' do
+      it 'shold be valid' do
         expect(user).to be_valid
       end
-      it 'should not be too long' do
-        user.name = 'a' * 21
-        expect(user).not_to be_valid
+      describe 'name' do
+        it 'should be proper long' do
+          user.name = 'a' * 20
+          expect(user).to be_valid
+        end
+        it 'should not be too long' do
+          user.name = 'a' * 21
+          expect(user).not_to be_valid
+        end
+      end
+      describe 'email' do
+        it 'should be proper long' do
+          user.email = 'a' * 243 + '@example.com'
+          expect(user).to be_valid
+        end
+        it 'should not be too long' do
+          user.email = 'a' * 244 + '@example.com'
+          expect(user).not_to be_valid
+        end
+        it 'addresses should be unique' do
+          duplicate_user = build(:user, email: user.email)
+          user.save
+          expect(duplicate_user).not_to be_valid
+        end
       end
     end
-    describe 'email' do
-      # it 'should be present' do
-      #   user.email = ''
-      #   expect(user).not_to be_valid
-      # end
-      it 'should be proper long' do
-        user.email = 'a' * 243 + '@example.com'
-        expect(user).to be_valid
+  end
+  describe 'update' do
+    let(:temp_user) { create(:temp_user, :with_area) }
+    let(:params) { attributes_for(:formal_user) }
+    describe 'Validates' do
+      it 'shold be valid' do
+        temp_user.update!(params)
+        expect(temp_user.errors).to be_empty
       end
-      it 'should not be too long' do
-        user.email = 'a' * 244 + '@example.com'
-        expect(user).not_to be_valid
+      describe 'email' do
+        it 'should be present' do
+          params[:email] = ''
+          expect{ temp_user.update!(params) }.to raise_error ActiveRecord::RecordInvalid
+        end
       end
-      it 'addresses should be unique' do
-        duplicate_user = build(:user, email: user.email)
-        user.save
-        expect(duplicate_user).not_to be_valid
+      describe 'password' do
+        it 'should be present' do
+          params[:password] = ''
+          expect{ temp_user.update!(params) }.to raise_error ActiveRecord::RecordInvalid
+        end
+        it 'confirmation should be present' do
+          params[:password_confirmation] = ''
+          expect{ temp_user.update!(params) }.to raise_error ActiveRecord::RecordInvalid
+        end
+        it 'not should be invalid' do
+          params[:password_confirmation] = 'pass'
+          expect{ temp_user.update!(params) }.to raise_error ActiveRecord::RecordInvalid
+        end
       end
-      # it 'addresses should be alphabetically unique' do
-      #   user = create(:user, email: 'JO@examPle.COM')
-      #   duplicate_user = build(:user, email: user.email.downcase)
-      #   expect(duplicate_user).not_to be_valid
-      # end
-      # it 'saves only downcase email' do
-      #   user = create(:user, email: 'JO@examPle.COM')
-      #   expect(user.reload.email).to eq 'jo@example.com'
-      # end
-    end
-    describe 'password' do
-      # it 'should be present' do
-      #   user.password = ''
-      #   user.password_confirmation = ''
-      #   expect(user).not_to be_valid
-      # end
-      # it 'confirmation should be present' do
-      #   user.password_confirmation = ''
-      #   expect(user).not_to be_valid
-      # end
-      # it 'not should be invalid' do
-      #   user.password = 'password'
-      #   user.password_confirmation = 'pass'
-      #   expect(user).not_to be_valid
-      # end
     end
   end
 end
