5818c843b1c2d28992aa33fa132abf49278d537a
necocoa <joyds217@gmail.com>
2020-02-21 14:34:26 +0900

    Change users NN ture(email, pass, gender)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_055407) do
+ActiveRecord::Schema.define(version: 2020_02_21_052716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,12 +75,12 @@ ActiveRecord::Schema.define(version: 2020_02_14_055407) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password_digest", null: false
+    t.string "email"
+    t.string "password_digest"
     t.string "token", null: false
     t.string "name"
     t.integer "birth_year", null: false
-    t.integer "gender", null: false
+    t.integer "gender"
     t.bigint "area_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
