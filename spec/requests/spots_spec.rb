49fddce9e45b30d1659a47af34f38cde8da78151
necocoa <joyds217@gmail.com>
2020-02-24 21:01:37 +0900

    Add spots spec

--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -2,16 +2,21 @@ require 'rails_helper'
 require 'application_helper'
 
 RSpec.describe 'Spots', type: :request do
-    describe 'get /v1/spots/{spot_id}' do
-        before do
-          get '/v1/spots/{spot_id}'
-        end
-        #it 'response is ok' do
-        #  expect(response).to be_ok
-        #end
-        #it "returns nil test" do
-        #  body = JSON.parse(response.body)
-        #  expect(body["message"]).to eq nil
-        #end
+  describe '/v1' do
+    let(:user) { create(:user, :with_area) }
+    let(:spot) { create(:spot) }
+    let(:set_not_exist_id) { @spot_id = 0 }
+    before do
+      @spot_id = spot.id
+      @options ||= {}
+      @options['HTTP_AUTHORIZATION'] = "Bearer #{user.token}"
     end
+    describe '/spots/:id' do
+      describe 'GET'do
+        subject { get "/v1/spots/#{@spot_id}", headers: @options }
+        it { is_expected.to eq 200 }
+        it_behaves_like ':idがおかしい時', exist: true
+      end
+    end
+  end
 end
