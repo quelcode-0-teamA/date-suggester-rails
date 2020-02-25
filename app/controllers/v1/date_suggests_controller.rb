c55e11b8306aa7a133ceed75c16459ee79878307
necocoa <joyds217@gmail.com>
2020-02-25 13:41:11 +0900

    Add authorize, user params

--- app/controllers/v1/date_suggests_controller.rb
+++ app/controllers/v1/date_suggests_controller.rb
@@ -1,6 +1,10 @@
 module V1
   class DateSuggestsController < ApplicationController
+    before_action :authorize!
+
     def suggest
+      suggest_params[:user_area] = @current_user.area_id
+      suggest_params[:birth_year] = @current_user.birth_year
       suggest_plan = Plan.suggest(suggest_params)
       render_serializer(suggest_plan, PlanDitailSerializer)
     end
