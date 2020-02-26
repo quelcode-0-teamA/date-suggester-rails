f5263f77759d2e26742d66264219a9143bc5e609
necocoa <joyds217@gmail.com>
2020-02-26 15:12:07 +0900

    suggestの例外時エラー文追加

--- app/controllers/v1/date_suggests_controller.rb
+++ app/controllers/v1/date_suggests_controller.rb
@@ -6,6 +6,12 @@ module V1
       suggest_params[:user_area] = @current_user.area_id
       suggest_params[:birth_year] = @current_user.birth_year
       suggest_plan = Plan.suggest(suggest_params)
+      if suggest_plan.blank?
+        render json: {
+          'message': 'Not Found',
+          'errors': '検索結果が見つかりませんでした。'
+        }.to_json, status: :not_found and return
+      end
       render json: suggest_plan
     end
 
@@ -20,7 +26,7 @@ module V1
         keys = %i[date_area date_budget date_time date_type]
         keys.each do |key|
           unless  query.key?(key)
-            raise ActionController::ParameterMissing, 'パラメータが不足しています'
+            raise ActionController::ParameterMissing, "#{key} の必須パラメータが送られていません。"
           end
         end
       end
