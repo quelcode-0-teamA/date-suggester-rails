a3fc0c5a91379e760488020dbb8a0ecea535003c
necocoa <joyds217@gmail.com>
2020-02-28 15:14:14 +0900

    マイ情報をmypage/userに変更した

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'v1/top#index'
   namespace :v1, format: 'json' do
     # Users
-    resources :users, only: %i[show update destroy]
+    resources :users, only: %i[show]
     post 'temp_sign_up', to: 'users#create_temp_user'
     post 'formal_sign_up', to: 'users#update_from_temp_to_formal'
     post 'login', to: 'users#login'
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
     get 'date-suggest', to: 'date_suggests#suggest'
     # MyPlans
     namespace :mypage do
+      get :user, to: 'user#show'
+      put :user, to: 'user#update'
+      delete :user, to: 'user#destroy'
       resources :my_plans, only: %i[index create show destroy]
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -62,37 +62,37 @@ RSpec.describe 'Users', type: :request do
         @user_id = user.id
         @options = { HTTP_AUTHORIZATION: "Bearer #{user.token}" }
       end
-      describe '/users/:id' do
-        describe 'GET' do
-          subject { get "/v1/users/#{@user_id}", headers: @options }
-          it { is_expected.to eq 200 }
-          it_behaves_like 'Tokenがおかしい時', exist: true
-          it_behaves_like ':idがおかしい時', exist: true
-        end
-        describe 'PUT' do
-          subject { put "/v1/users/#{@user_id}", headers: @options, params: @params }
-          before do
-            @params = {
-              "user": {
-                "name": 'edit name',
-                "birth_year": 2000,
-                "gender": 'male',
-                "area_id": area_id,
-              }
-            }
-          end
-          it { is_expected.to eq 200 }
-          it_behaves_like 'Tokenがおかしい時', exist: true, different: true
-          it_behaves_like ':idがおかしい時', exist: true
-        end
-        describe 'DELETE' do
-          subject { delete "/v1/users/#{@user_id}", headers: @options }
-          it { is_expected.to eq 204 }
-          it { expect { subject }.to change(User, :count).by(-1) }
-          it_behaves_like 'Tokenがおかしい時', exist: true, different: true
-          it_behaves_like ':idがおかしい時', exist: true
-        end
-      end
+      # describe '/users/:id' do
+      #   describe 'GET' do
+      #     subject { get "/v1/users/#{@user_id}", headers: @options }
+      #     it { is_expected.to eq 200 }
+      #     it_behaves_like 'Tokenがおかしい時', exist: true
+      #     it_behaves_like ':idがおかしい時', exist: true
+      #   end
+      #   describe 'PUT' do
+      #     subject { put "/v1/users/#{@user_id}", headers: @options, params: @params }
+      #     before do
+      #       @params = {
+      #         "user": {
+      #           "name": 'edit name',
+      #           "birth_year": 2000,
+      #           "gender": 'male',
+      #           "area_id": area_id,
+      #         }
+      #       }
+      #     end
+      #     it { is_expected.to eq 200 }
+      #     it_behaves_like 'Tokenがおかしい時', exist: true, different: true
+      #     it_behaves_like ':idがおかしい時', exist: true
+      #   end
+      #   describe 'DELETE' do
+      #     subject { delete "/v1/users/#{@user_id}", headers: @options }
+      #     it { is_expected.to eq 204 }
+      #     it { expect { subject }.to change(User, :count).by(-1) }
+      #     it_behaves_like 'Tokenがおかしい時', exist: true, different: true
+      #     it_behaves_like ':idがおかしい時', exist: true
+      #   end
+      # end
     end
   end
 end
