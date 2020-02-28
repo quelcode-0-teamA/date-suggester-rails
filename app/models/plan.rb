e43a4bb28b8043aa2373d92baeefb5668088ed32
necocoa <joyds217@gmail.com>
2020-02-28 13:34:33 +0900

    ソート条件をリージョンに変更した - user_areaのリージョン読み込み - self. のリファクタリング - メソッドを細かくした

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -7,47 +7,52 @@ class Plan < ApplicationRecord
     update(total_budget: total_budget)
   end
 
-  def self.suggest(params)
-    budget_range = get_date_budget(params[:birth_year], params[:date_budget].to_i)
-    sort_params = {
-      budget_range: budget_range,
-      user_area: params[:user_area],
-      date_area: params[:date_area],
-      date_time: params[:date_time],
-      date_type: params[:date_type]
-    }
-    sorted_plans = sort_plans(sort_params)
-    sorted_plans.sample
-  end
+  class << self
+    def suggest(params)
+      sorted_plans = sort_plans(params)
+      sorted_plans.sample
+    end
 
-  def self.sort_plans(sort_params)
-    Plan.where(total_budget: sort_params[:budget_range][0]...sort_params[:budget_range][1])
-        .where(area_id: sort_params[:user_area])
-  end
+    def sort_plans(params)
+      budget_range = get_date_budget(params[:birth_year], params[:date_budget].to_i)
+      areas = get_areas(params[:user_area])
+      Plan.where(total_budget: budget_range[0]...budget_range[1])
+          .where(area: areas)
+    end
 
-  def self.get_date_budget(birth_year, date_budget)
-    tens_place_age, early_or_late = trans_age(birth_year)
-    standard_budget = UserType.get_budget(tens_place_age, early_or_late)
-    date_budget(standard_budget, date_budget)
-  end
+    def get_date_budget(birth_year, date_budget)
+      standard_budget = get_standard_budget(birth_year)
+      date_budget(standard_budget, date_budget)
+    end
 
-  def self.trans_age(birth_year)
-    age = Time.current.year - birth_year
-    tens_place_age = age / 10
-    early_or_late = age % 10 < 5 ? 0 : 1
-    [tens_place_age, early_or_late]
-  end
+    def get_standard_budget(birth_year)
+      tens_place_age, early_or_late = trans_age(birth_year)
+      UserType.get_budget(tens_place_age, early_or_late)
+    end
 
-  def self.date_budget(standard_budget, date_budget)
-    case date_budget
-    when 0
-      [(standard_budget * 0).round, (standard_budget * 0.7).round]
-    when 1
-      [(standard_budget * 0.7).round, (standard_budget * 2).round]
-    when 2
-      [(standard_budget * 2).round, (standard_budget * 4).round]
-    else
-      raise ActionController::ParameterMissing, 'date_budget の値が異常です'
+    def trans_age(birth_year)
+      age = Time.current.year - birth_year
+      tens_place_age = age / 10
+      early_or_late = age % 10 < 5 ? 0 : 1
+      [tens_place_age, early_or_late]
+    end
+
+    def date_budget(standard_budget, date_budget)
+      case date_budget
+      when 0
+        [(standard_budget * 0).round, (standard_budget * 0.7).round]
+      when 1
+        [(standard_budget * 0.7).round, (standard_budget * 2).round]
+      when 2
+        [(standard_budget * 2).round, (standard_budget * 4).round]
+      else
+        raise ActionController::ParameterMissing, 'date_budget の値が異常です'
+      end
+    end
+
+    def get_areas(user_area)
+      area = Area.find(user_area)
+      Area.where(region: area.region)
     end
   end
   private_class_method :get_date_budget, :trans_age, :date_budget
