9dbd66d077ba7c0afa1f3ae00bf4789698673f4a
necocoa <joyds217@gmail.com>
2020-03-01 19:43:32 +0900

    Fix seed-fu data

--- db/fixtures/development/05_plan_spots.rb
+++ db/fixtures/development/05_plan_spots.rb
@@ -1,16 +1,16 @@
 spot_cnt = 1
 areas_count = Area.where('"order" > ?', 0).count
 plans_count = areas_count * 10
-(1..plans_count).each do |i|
-  2.times do |n|
+(0...plans_count).each do |i|
+  (1..3).each do |n|
     spot_cnt = 1 if spot_cnt > 20
     PlanSpot.seed do |s|
-      s.id = i + n
-      s.plan_id = i
-      s.spot_id = spot_cnt # 1..10
+      s.id = (i * 3) + n
+      s.plan_id = i + 1
+      s.spot_id = spot_cnt
       s.order = n
     end
     spot_cnt += 1
   end
-  Plan.find(i).recalculation_total_budget
+  Plan.find(i + 1).recalculation_total_budget
 end
