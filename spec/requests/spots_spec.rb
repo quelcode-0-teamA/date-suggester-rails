8cf2026f6ee7c43f59edcebe07e8a31405084464
necocoa <joyds217@gmail.com>
2020-03-08 17:32:03 +0900

    beforeからletのオーバーライドにリファクタリング

--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -4,17 +4,17 @@ require 'application_helper'
 RSpec.describe 'Spots', type: :request do
   describe '/v1' do
     let(:user) { create(:user) }
+    let(:options) { { HTTP_AUTHORIZATION: "Bearer #{user.token}" } }
     let(:spot) { create(:spot) }
-    let(:set_not_exist_id) { @spot_id = 0 }
-    before do
-      @spot_id = spot.id
-      @options = { HTTP_AUTHORIZATION: "Bearer #{user.token}" }
-    end
+    let(:spot_id) { spot.id }
     describe '/spots/:id' do
       describe 'GET' do
-        subject { get "/v1/spots/#{@spot_id}", headers: @options }
+        subject { get "/v1/spots/#{spot_id}", headers: options }
         it { is_expected.to eq 200 }
-        it_behaves_like ':idがおかしい時', exist: true
+        context ':idが存在しない時' do
+          let(:spot_id) { 0 }
+          it { is_expected.to eq 404 }
+        end
       end
     end
   end
